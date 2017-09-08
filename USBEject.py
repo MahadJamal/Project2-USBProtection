247ab8f5ece03c5084ed61554cf0c82560769fc9
Mahad Jamal <mjamal@bionx.local>
2017-09-08 13:23:14 -0400

    WIP

---
+++ USBEject.py
@@ -0,0 +1,64 @@
+from time import sleep
+import subprocess
+import Password_GUI
+
+
+class USBEJECT():
+    USBDisk = []
+    makeGUI = 0
+    def __init__(self):
+        self.label = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.monitorDisk = []
+        self.isININ = False
+
+
+    def USBEJECT_initialdrives(self, label, monitorDisk):
+        for i in label:
+            try:
+                file = open(i + ':/')
+            except Exception as e:
+                '''
+                error = 2  =>not found
+                error = 13 =>permission denied (exist!)
+                '''
+                if (e.errno == 13):
+                    print("Disk : " + i + " Exist!")
+                else:
+                    monitorDisk.append(i)
+
+
+    def USBEJECT_monitorUSB(self, monitorDisk, isININ):
+        print("Start monitoring.....")
+        while (True):
+            print("Check...")
+            disk = '';
+            for i in monitorDisk:
+                try:
+                    file = open(i + ':/')
+                except Exception as e:
+                    if (e.errno == 13):
+                        USBEJECT.USBDisk.append(i)
+                        USBEJECT.makeGUI = 1
+                        if (Password_GUI.AuthSuccess):
+                            self.isININ = False
+                            USBEJECT.makeGUI = 0
+                            break
+                        else:
+                            print("Disk : " + i + " Added!")
+                            self.isININ = True
+                            print(self.isININ)
+                            disk = i
+                            break
+            sleep(1)
+
+    def USBEJECT_ejector(self, USBDisk, isININ):
+        print('Ejector Started')
+        if (isININ):
+            tmpFile = open('tmp.ps1', 'w')
+            tmpFile.write('$driveEject = New-Object -comObject Shell.Application\n')
+            tmpFile.write('$driveEject.Namespace(17).ParseName("' + USBDisk[0] + ':").InvokeVerb("Eject")')
+            tmpFile.close()
+            process = subprocess.Popen(['powershell.exe', '-ExecutionPolicy', 'Unrestricted', './tmp.ps1'])
+            process.communicate()
+
